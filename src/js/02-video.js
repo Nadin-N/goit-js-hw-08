f0f37a7e3d23bacd4d3c8709b542e2200032e34f
Nadin Severin <nadinlikes2018@gmail.com>
2022-12-30 01:46:24 +0200

    add hw8 files

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+import localStorageService from './helper';
+const { saveToLS, loadFromLS } = localStorageService;
 
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+let videoPlaybackTime;
+
+function onPlay(event) {
+  videoPlaybackTime = event.seconds;
+  saveToLS(LOCAL_STORAGE_KEY, videoPlaybackTime);
+  return videoPlaybackTime;
+}
+
+player
+  .setCurrentTime(loadFromLS(LOCAL_STORAGE_KEY))
+  .then(function (seconds) {
+    console.log(`відновлення відтворення з ${seconds} секунди`);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,68 @@
+import throttle from 'lodash.throttle';
+import localStorageService from './helper';
+const { saveToLS, loadFromLS, removeFromLS } = localStorageService;
 
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  fitBackFormEl: document.querySelector('.feedback-form'),
+  fields: document
+    .querySelector('.feedback-form')
+    .querySelectorAll('input, textarea'),
+};
+
+refs.fitBackFormEl.addEventListener(
+  'input',
+  throttle(onFormElementsInput, 500)
+);
+refs.fitBackFormEl.addEventListener('submit', onFormSubmit);
+
+let savedObject = {};
+let userFitBack = {};
+
+fillInFormData();
+
+function fillInFormData() {
+  savedObject = loadFromLS(LOCAL_STORAGE_KEY);
+
+  if (savedObject) {
+    for (const prop in savedObject) {
+      refs.fitBackFormEl.elements[prop].value = savedObject[prop];
+    }
+    createUser();
+  }
+}
+
+function createUser() {
+  const data = new FormData(refs.fitBackFormEl);
+
+  for (const [key, value] of data.entries()) {
+    userFitBack[key] = value;
+  }
+  return userFitBack;
+}
+
+function onFormElementsInput(event) {
+  createUser();
+
+  saveToLS(LOCAL_STORAGE_KEY, userFitBack);
+
+  return userFitBack;
+}
+
+function onFormSubmit(event) {
+  for (const el of refs.fields) {
+    if (el.value === '') {
+      alert('Заповніть всі поля форми, будь ласка!');
+      return;
+    }
+  }
+
+  event.preventDefault();
+
+  event.target.reset();
+
+  removeFromLS(LOCAL_STORAGE_KEY);
+
+  console.log(userFitBack);
+}
